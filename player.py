3a47abf56bf0e6713ba626a00642ab46efdb971f
shion0325 <s243167@stn.nagaokaut.ac.jp>
2024-09-24 16:53:15 +0900

    ADD player.py

---
+++ player.py
@@ -0,0 +1,16 @@
+
+class player:
+    def __init__(self):
+        player.x = 0
+        player.y = 0
+
+    def move(self,moves):
+        if moves == 0:
+            player.x += 1
+        if moves == 1:
+            player.x += -1
+        if moves == 2:
+            player.y += 1
+        if moves == 1:
+            player.y += -1
+         
